b3e18d4753f043e68b7f79db408dba844fbae9c7
Mohamed Lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-09-30 21:44:04 +0100

    Added a dummy movies list

---
+++ src/shared/movies.js
@@ -0,0 +1,123 @@
+export const MOVIES = 
+  [
+    {
+      id: 0,
+      name: 'The Matrix',
+      category: 'Action',
+      year: 2000,
+      poster: 'https://images-na.ssl-images-amazon.com/images/I/51oBxmV-dML._AC_.jpg',
+      description: 'Thomas Anderson, a computer programmer, is led to fight an underground war against powerful computers who have constructed his entire reality with a system called the Matrix.',
+      trailer: 'https://www.youtube.com/watch?v=vKQi3bBA1y8',
+      url: 'https://www.youtube.com/watch?v=vKQi3bBA1y8'
+    },
+    {
+      id: 1,
+      name: 'The Dark Knight Rises',
+      category: 'Action',
+      year: 2012,
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_.jpg',
+      description: 'Bane, an imposing terrorist, attacks Gotham City and disrupts its eight-year-long period of peace. This forces Bruce Wayne to come out of hiding and don the cape and cowl of Batman again.',
+      trailer: 'https://www.youtube.com/watch?v=g8evyE9TuYk',
+      url: 'https://www.youtube.com/watch?v=g8evyE9TuYk'
+    },
+    {
+      id: 2,
+      name: 'The Maze Runner',
+      category: 'Adventure',
+      year: 2014,
+      poster: 'https://images.moviesanywhere.com/00c4dae168a9e85bbbe482392274e3ab/b63c2858-6cc6-4fd1-8b46-8c16db96891c.jpg',
+      description: 'Thomas loses his memory and finds himself trapped in a massive maze called the Glade. He and his friends try to escape from the maze and eventually learn that they are subjects of an experiment.',
+      trailer: 'https://www.youtube.com/watch?v=AwwbhhjQ9Xk',
+      url: 'https://www.youtube.com/watch?v=AwwbhhjQ9Xk'
+    },
+    {
+      id: 3,
+      name: 'The Matrix',
+      category: 'Action',
+      year: 2000,
+      poster: 'https://images-na.ssl-images-amazon.com/images/I/51oBxmV-dML._AC_.jpg',
+      description: 'Thomas Anderson, a computer programmer, is led to fight an underground war against powerful computers who have constructed his entire reality with a system called the Matrix.',
+      trailer: 'https://www.youtube.com/watch?v=vKQi3bBA1y8',
+      url: 'https://www.youtube.com/watch?v=vKQi3bBA1y8'
+    },
+    {
+      id: 4,
+      name: 'The Dark Knight Rises',
+      category: 'Action',
+      year: 2012,
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_.jpg',
+      description: 'Bane, an imposing terrorist, attacks Gotham City and disrupts its eight-year-long period of peace. This forces Bruce Wayne to come out of hiding and don the cape and cowl of Batman again.',
+      trailer: 'https://www.youtube.com/watch?v=g8evyE9TuYk',
+      url: 'https://www.youtube.com/watch?v=g8evyE9TuYk'
+    },
+    {
+      id: 5,
+      name: 'The Maze Runner',
+      category: 'Adventure',
+      year: 2014,
+      poster: 'https://images.moviesanywhere.com/00c4dae168a9e85bbbe482392274e3ab/b63c2858-6cc6-4fd1-8b46-8c16db96891c.jpg',
+      description: 'Thomas loses his memory and finds himself trapped in a massive maze called the Glade. He and his friends try to escape from the maze and eventually learn that they are subjects of an experiment.',
+      trailer: 'https://www.youtube.com/watch?v=AwwbhhjQ9Xk',
+      url: 'https://www.youtube.com/watch?v=AwwbhhjQ9Xk'
+    },
+    {
+      id: 6,
+      name: 'The Matrix',
+      category: 'Action',
+      year: 2000,
+      poster: 'https://images-na.ssl-images-amazon.com/images/I/51oBxmV-dML._AC_.jpg',
+      description: 'Thomas Anderson, a computer programmer, is led to fight an underground war against powerful computers who have constructed his entire reality with a system called the Matrix.',
+      trailer: 'https://www.youtube.com/watch?v=vKQi3bBA1y8',
+      url: 'https://www.youtube.com/watch?v=vKQi3bBA1y8'
+    },
+    {
+      id: 7,
+      name: 'The Dark Knight Rises',
+      category: 'Action',
+      year: 2012,
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_.jpg',
+      description: 'Bane, an imposing terrorist, attacks Gotham City and disrupts its eight-year-long period of peace. This forces Bruce Wayne to come out of hiding and don the cape and cowl of Batman again.',
+      trailer: 'https://www.youtube.com/watch?v=g8evyE9TuYk',
+      url: 'https://www.youtube.com/watch?v=g8evyE9TuYk'
+    },
+    {
+      id: 8,
+      name: 'The Maze Runner',
+      category: 'Adventure',
+      year: 2014,
+      poster: 'https://images.moviesanywhere.com/00c4dae168a9e85bbbe482392274e3ab/b63c2858-6cc6-4fd1-8b46-8c16db96891c.jpg',
+      description: 'Thomas loses his memory and finds himself trapped in a massive maze called the Glade. He and his friends try to escape from the maze and eventually learn that they are subjects of an experiment.',
+      trailer: 'https://www.youtube.com/watch?v=AwwbhhjQ9Xk',
+      url: 'https://www.youtube.com/watch?v=AwwbhhjQ9Xk'
+    },
+    {
+      id: 9,
+      name: 'The Matrix',
+      category: 'Action',
+      year: 2000,
+      poster: 'https://images-na.ssl-images-amazon.com/images/I/51oBxmV-dML._AC_.jpg',
+      description: 'Thomas Anderson, a computer programmer, is led to fight an underground war against powerful computers who have constructed his entire reality with a system called the Matrix.',
+      trailer: 'https://www.youtube.com/watch?v=vKQi3bBA1y8',
+      url: 'https://www.youtube.com/watch?v=vKQi3bBA1y8'
+    },
+    {
+      id: 10,
+      name: 'The Dark Knight Rises',
+      category: 'Action',
+      year: 2012,
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_.jpg',
+      description: 'Bane, an imposing terrorist, attacks Gotham City and disrupts its eight-year-long period of peace. This forces Bruce Wayne to come out of hiding and don the cape and cowl of Batman again.',
+      trailer: 'https://www.youtube.com/watch?v=g8evyE9TuYk',
+      url: 'https://www.youtube.com/watch?v=g8evyE9TuYk'
+    },
+    {
+      id: 11,
+      name: 'The Maze Runner',
+      category: 'Adventure',
+      year: 2014,
+      poster: 'https://images.moviesanywhere.com/00c4dae168a9e85bbbe482392274e3ab/b63c2858-6cc6-4fd1-8b46-8c16db96891c.jpg',
+      description: 'Thomas loses his memory and finds himself trapped in a massive maze called the Glade. He and his friends try to escape from the maze and eventually learn that they are subjects of an experiment.',
+      trailer: 'https://www.youtube.com/watch?v=AwwbhhjQ9Xk',
+      url: 'https://www.youtube.com/watch?v=AwwbhhjQ9Xk'
+    }
+  ]
