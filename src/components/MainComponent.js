041cdf38edbbc2c2fa21c332bb575d6a20ff68de
Mohamed Lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-09-30 21:39:17 +0100

    Added Main component

---
+++ src/components/MainComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './HeaderComponent';
+import MediaList from './MediaListComponent';
+
+function Main() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default Main;
