c7c9c04b5cf86a2867799d7aaa9b10b3c126ec37
Mohamed Lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-09-30 23:26:23 +0100

    Added Home component

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,14 @@
+import { Container, Typography } from '@material-ui/core';
+import React from 'react';
+
+function Home() {
+  return (
+    <Container fixed>
+      <Typography variant='h4'>
+        Home
+      </Typography>
+    </Container>
+  );
+}
+
+export default Home;
